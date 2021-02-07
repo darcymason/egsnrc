50b6bc53cab9b03def7ca7edbf44281294ce446a
darcymason <darcymason@gmail.com>
2021-02-06 23:39:49 -0500

    Hand-editing on electr * add particle-selection callbacks * add params back for flags * many -1's for 0-based arrays * remove inlines of functions that now have Python versions * fix indentations

---
+++ egsnrc/params.py
@@ -0,0 +1,21 @@
+import numpy
+
+EPSEMFP: numpy.float64 = 1.E-8
+RANDOMIZE_TUSTEP: bool = False
+PRESTA_II: numpy.int32 = 0
+EM_MACROS_ACTIVE: bool = False
+TRANAUSB: numpy.int32 = 0
+TRANAUSA: numpy.int32 = 5
+MOLLAUSB: numpy.int32 = 8
+MOLLAUSA: numpy.int32 = 9
+BHABAUSB: numpy.int32 = 10
+BHABAUSA: numpy.int32 = 11
+ANNIHFAUSB: numpy.int32 = 12
+ANNIHFAUSA: numpy.int32 = 13
+BREMAUSB: numpy.int32 = 6
+BREMAUSA: numpy.int32 = 7
+EGSCUTAUS: numpy.int32 = 1
+PEGSCUTAUS: numpy.int32 = 2
+ANNIHRAUSB: numpy.int32 = 28
+ANNIHRAUSA: numpy.int32 = 14
+USERDAUS: numpy.int32 = 3
