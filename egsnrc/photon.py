98381f6c98d456672f61d90b67bd630316efbff2
darcymason <darcymason@gmail.com>
2023-01-06 17:30:11 -0500

    Refactor and set up toy1 pytorch example * random and Particles can be numpy or pytorch * toy1 example - just change energy - runs on colab if import egsnrc

--- egsnrc/photon.py
+++ egsnrc/photon.py
@@ -1,16 +1,28 @@
 import numpy
 
+from egsnrc.vect import *  # XXX eventually do selective import
+from egsnrc import random
 
-def photon(fparticles, iparticles, howfar, ausgab):
+def toy(particles, key):
+    key, ran_floats = random.floats_0_1(key, len(particles.energy))
+    energy = particles.energy - 5.0 * ran_floats
+    status_mask = energy <= 0
+    particles = particles.cut(status_mask)
+    particles = particles.set(energy=energy[status_mask])
+
+    return particles, key
+
+
+def photon(particles, regions, media, scoring, howfar, ausgab):
     """Called from `shower` to track photons
 
     Parameters
     ----------
-    fparticles: numpy.ndarray
-        2D array of photon states (float64) x NUM_PARTICLES
+    particles: Particles
+        Particles class with all particle state contained
 
-    iparticles: numpy.ndarray
-        2D array of photon states (int32) x NUM_PARTICLES
+    media: Media
+        Media class with access to medium information
 
     howfar:  function
         Callback to `howfar` user-code method
@@ -24,57 +36,53 @@ def photon(fparticles, iparticles, howfar, ausgab):
         1 for a normal return
     """
 
-    iparticles[STATUS] = 0 # set up normal return
-
     PCUT_DISCARD = 1
     USER_PHOTON_DISCARD = 2
     PAIR_ELECTRONS_KILLED = 3
 
-    # Keep track of particles with no outcome yet, start with all of them
-    falive = fparticles
-    ialive = iparticles
-
-    while True:
+    while len(particles):  # one "step" for each photon, either interacting or reaching boundary
         # if eig <= pcut[irl_m1]: particle_outcome = PCUT_DISCARD
-        iparticles[STATUS,fparticles[ENERGY] < pcut[iparticles[REGION]]] = PCUT_DISCARD
+        pcut_mask = particles.energy < pcut[particles.region]
+        ialive[STATUS, pcut_mask] = PCUT_DISCARD
 
         # if wt == 0.0:  particle_outcome = USER_PHOTON_DISCARD
-        iparticles[STATUS, fparticles[WT] == 0] = USER_PHOTON_DISCARD
+        wt0_mask = falive[WT] == 0
+        ialive[STATUS, wt0_mask] = USER_PHOTON_DISCARD
 
-        alive = (iparticles[STATUS] == 0)  # could use ialive[STATUS] if don't add particles
-        falive = fparticles[:, alive]
-        ialive = iparticles[:, alive]
-
-        if falive.size == 0:
-            break  # all particles have an outcome, we're done this set
+        # ASSUME (for now) that all particles at top of loop are alive
+        #    later in loop should remove any that are not
+        # alive_mask = iparticles[STATUS] == 0  # could use ialive[STATUS] if don't add particles
+        # falive = fparticles[:, alive_mask]
+        # ialive = iparticles[:, alive_mask]
 
         gle = numpy.log(falive[ENERGY])
 
-        #    here to sample no. mfp to transport before interacting
-
+        # Sample number of mfp to transport before interacting
         rnno35 = rng.random(falive.shape[1])
         rnno35[rnno35==0] = 1.0e-30  # could use `numpy.clip` but not quite same as old code
         dpmfp = -numpy.log(rnno35)
 
+        # XXX do not special case vacuum (at least for now) - just use very low rho
         # if medium != 0:
-        non_vac = (ialive[MEDIUM] != 0)
-        non_vac_mediums = ialive[MEDIUM, non_vac]
-        # set interval gle, ge;
-        # lgle is integer index into upcoming arrays
-        lgle = (
-            ge1[non_vac_mediums]*gle + ge0[non_vac_mediums]
-        ).astype(numpy.int32)
+        # non_vac = (ialive[MEDIUM] != 0)
+        # non_vac_mediums = ialive[MEDIUM, non_vac]
+        # # set interval gle, ge;
+        # # lgle is integer index into upcoming arrays
+        # lgle = (
+        #     ge1[non_vac_mediums]*gle + ge0[non_vac_mediums]
+        # ).astype(numpy.int32)
 
-        # xxxx
-        # xxxx
-        # ******* continue ... ************
+        media = ialive[MEDIUM]
+        # Calc integer index into sigma arrays
+        lgle = (ge1[media] * gle + ge0[media]).astype(numpy.int32)
+
 
         # evaluate gmfpr0 using gmfp(gle)
         # Appears to do (if not sin)
         # {P1}={P2}1(L{P3},MEDIUM)*{P3}+{P2}0(L{P3},MEDIUM);
 
         # gmfpr0, 1 set in egs_scale_photon_xsection() in EGSnrc orig
-        gmfpr0 = gmfp1(Lgle, MEDIUM) * gle + gmfp0(Lgle, MEDIUM)
+        gmfpr0 = gmfp1[MEDIUM, lgle] * gle + gmfp0[MEDIUM, lgle]
 
 
 
---
+++ egsnrc/random.py
@@ -0,0 +1,38 @@
+
+from numpy import random
+try:
+    import torch
+except ImportError:
+    pass
+
+# Numpy random functions
+def _np_initialize(seed):
+    random.seed(seed)
+    return seed
+
+
+def _np_floats_0_1(key, num, device=None):
+    return key, random.random(num)
+
+
+# PyTorch random funtions ---------
+def _torch_initialize(seed):
+    return torch.random.manual_seed(seed)
+
+
+def _torch_floats_0_1(key, num, device=None):
+    return key, torch.rand(num, device=device)
+
+
+# Configuration to select between libraries
+def set_array_library(lib: str):
+    global floats_0_1, initialize
+
+    if lib == "numpy":
+        floats_0_1 = _np_floats_0_1  # Later switch depending on using gpu arrays
+        initialize = _np_initialize
+    elif lib == "pytorch":
+        floats_0_1 = _torch_floats_0_1
+        initialize = _torch_initialize
+    else:
+        raise NotImplementedError("Array library not currently handled")
