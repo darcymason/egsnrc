48ec3d7702fae5e403e67e6b2bf5fb86fd837483
darcymason <darcymason@gmail.com>
2023-02-19 22:37:37 -0500

    Add missing two_slab.py from last commit

--- egsnrc/tests/data/two_slab.py
+++ egsnrc/tests/data/two_slab.py
@@ -34,10 +34,19 @@ SCORE_nCOMPTON, SCORE_nPHOTO, SCORE_nLOST, SCORE_nCOMPTINDIRECT, SCORE_nPHOTOIND
 SCORE_eCOMPTON, SCORE_ePHOTO, SCORE_eLOST = np.arange(3, dtype=np.int32)
 
 
+@device_jit
+def get_source_particle(rng_states, gid, regions):
+    return Particle(
+        KINT(0), regions[1], KFLOAT(1.0),
+        KFLOAT(0), KFLOAT(0), KFLOAT(0),
+        KFLOAT(0), KFLOAT(0), KFLOAT(1),
+    )
+
 # Example of scoring function (ausgab)
 @device_jit
 def ausgab(gid, status, p1, p2, iscore, fscore):
     # Note, if don't track by gid, then need "atomic" operations to handle multi-thread
+    # return
     region_number = p2.region.number
     if status == COMPTON:
         if p1.energy == 1:
@@ -71,7 +80,7 @@ def howfar(p, regions, ustep):  # -> step, region, discard_flag (>0 to discard)
     #                |                 |                  |
     #               z=0              2 cm               4 cm
 
-
+    # return ustep, p.region, DISCARD  # XXX
     region = p.region
     region_num = region.number
     if region_num not in (1, 2):  # outside the geometry
@@ -100,25 +109,12 @@ If optional num_particles is not specified, it defaults to 50 for testing.
 If num_batches is not specified, it defaults to 1 on CPU or 2 on GPU (to separate compile time)
 """
 
-if __name__ == "__main__":
-    import sys
-    num_particles = 200
-    num_batches = 8
-    if len(sys.argv) > 1:
-        try:
-            num_particles = int(sys.argv[1])
-        except ValueError:
-            print("Optional num_particles command-line argument must be an integer")
-            print(usage)
-            sys.exit(-1)
-    if len(sys.argv) > 2:
-        try:
-            num_batches = int(sys.argv[2])
-        except ValueError:
-            print("Optional num_batches command-line argument must be an integer")
-            print(usage)
 
-
+def run(num_particles, num_batches):
+    print("Call to `run`")
+    global boundaries
+    global regions
+    global media
     # Set up the media and the regions
     # Match rho and sumA from Mortran run
     Ta = Medium(1, "Ta", rho=16.6, sumA=180.948)
@@ -140,11 +136,18 @@ if __name__ == "__main__":
 
     # Set up particles with random range of energies
     # starting at (0, 0, 0) at edge of Region 1, traveling in z direction
-    source = PhotonSource(
-        energy=1.0, region=regions[1],
-        position=(0, 0, 0), direction=(0, 0, 1),
-        store_particles=True  # for debugging with small num_particles
-    )
+    # particle_1MeV = Particle(
+    #         KINT(0), regions[1], KFLOAT(1.0),
+    #         KFLOAT(0), KFLOAT(0), KFLOAT(0),
+    #         KFLOAT(0), KFLOAT(0), KFLOAT(1),
+    #     )
+
+
+    # source = PhotonSource(
+    #     energy=1.0, region=regions[1],
+    #     position=(0, 0, 0), direction=(0, 0, 1),
+    #     store_particles=True  # for debugging with small num_particles
+    # )
     # Initialize Scoring
     NUM_REGIONS = 4
 
@@ -162,17 +165,17 @@ if __name__ == "__main__":
 
     # Start the simulation
     iscore, fscore = shower(
-        42, num_particles*num_batches, source, regions, media, howfar,
+        42, num_particles*num_batches, get_source_particle, regions, media, howfar,
         ausgab, iscore, fscore
     )
 
 
-    if len(fscore) < 51:
-        print("fparticle energies")
-        print(source.fparticles[:, ENERGY])
-        print("Scoring - Particles/Region")
-        print("nCompt     nPhoto     eCompt     ePhoto     nLost      eLost")
-        print(fscore)
+    # if len(fscore) < 51:
+    #     print("fparticle energies")
+    #     print(source.fparticles[:, ENERGY])
+    #     print("Scoring - Particles/Region")
+    #     print("nCompt     nPhoto     eCompt     ePhoto     nLost      eLost")
+    #     print(fscore)
     # for r in range(1, 3):
     #     print("Energy Total by Region")
     #     print(f"Region {r}----")
@@ -193,10 +196,14 @@ if __name__ == "__main__":
 
     ncompts_reg = [None]
     nphotos_reg = [None]  # 0th is blank
+    ncompts_ind_reg = [None]
+    nphotos_ind_reg = [None]
     # XXX ncomps_ind, nphotos_ind
     for r in range(1,3):
         ncompts = []
         nphotos = []
+        ncompts_ind = []
+        nphotos_ind = []
         for i_batch in range(num_batches):
             i1 = i_batch * num_particles
             i2 = i1 + num_particles
@@ -207,14 +214,43 @@ if __name__ == "__main__":
             print(f"{r}\t{n_compt}\t{n_photo}\t{n_compt_ind}\t{n_photo_ind}")
             ncompts.append(n_compt)
             nphotos.append(n_photo)
+            ncompts_ind.append(n_compt_ind)
+            nphotos_ind.append(n_photo_ind)
         ncompts_reg.append(ncompts)
         nphotos_reg.append(nphotos)
+        ncompts_ind_reg.append(ncompts_ind)
+        nphotos_ind_reg.append(nphotos_ind)
 
     print("\nMeans and std devs")
     print("-------------------")
-    print("Region\tCompton\tPhoto")
+    print("Region\tCompton\t\tPhoto\t\tComptInd\t\tPhotoInd")
     for r in range(1,3):
         print(
-            f"{r}\t{np.mean(ncompts_reg[r])} +/- {np.std(ncompts_reg[r])}"
-            f"""\t{np.mean(nphotos_reg[r])} +- {np.std(nphotos_reg[r])}"""
+            f"{r}\t{np.mean(ncompts_reg[r]):5.1f} +/- {np.std(ncompts_reg[r]):5.1f}"
+            f"""\t{np.mean(nphotos_reg[r]):5.1f} +- {np.std(nphotos_reg[r]):5.1f}"""
+            f"{r}\t{np.mean(ncompts_ind_reg[r]):5.1f} +/- {np.std(ncompts_ind_reg[r]):5.1f}"
+            f"""\t{np.mean(nphotos_ind_reg[r]):5.1f} +- {np.std(nphotos_ind_reg[r]):5.1f}"""
         )
+
+    return ncompts_reg, nphotos_reg, ncompts_ind_reg, nphotos_ind_reg
+
+
+if __name__ == "__main__":
+    import sys
+    num_particles = 200
+    num_batches = 8
+    if len(sys.argv) > 1:
+        try:
+            num_particles = int(sys.argv[1])
+        except ValueError:
+            print("Optional num_particles command-line argument must be an integer")
+            print(usage)
+            sys.exit(-1)
+    if len(sys.argv) > 2:
+        try:
+            num_batches = int(sys.argv[2])
+        except ValueError:
+            print("Optional num_batches command-line argument must be an integer")
+            print(usage)
+
+    run(num_particles, num_batches)
